206d0486f700d07fdb9d651af3ba1d312c1500b9
Ilya Khorobrykh <horobryh17@gmail.com>
2022-08-14 02:28:54 +0300

    Finished About Component

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import './About.css';
+import { Award, People, FolderCheck } from 'react-bootstrap-icons';
 
 import background from 'assets/img/background/color-sharp.png';
+import me from 'assets/img/me/me-about.jpg';
 import { ReturnComponentType } from 'types';
 
 export const About = (): ReturnComponentType => {
@@ -12,12 +14,41 @@ export const About = (): ReturnComponentType => {
                 <div className="row">
                     <div className="col-12">
                         <div className="about-bx">
-                            <h2>About</h2>
-                            <p>
-                                Lorem Ipsum is simply dummy text of the printing and
-                                typesetting industry.
-                                <br /> Lorem Ipsum has been the standard dummy text.
-                            </p>
+                            <h2>About Me</h2>
+                            <div className="about-container">
+                                <div className="about-me">
+                                    <div className="about-me-image">
+                                        <img src={me} alt="me" />
+                                    </div>
+                                </div>
+                                <div className="about-content">
+                                    <div className="about-cards">
+                                        <article className="about-card">
+                                            <Award className="about-icon" />
+                                            <h5>Experience</h5>
+                                            <small>3+ Years Working</small>
+                                        </article>
+                                        <article className="about-card">
+                                            <People className="about-icon" />
+                                            <h5>Clients</h5>
+                                            <small>200+ Worldwide</small>
+                                        </article>
+                                        <article className="about-card">
+                                            <FolderCheck className="about-icon" />
+                                            <h5>Projects</h5>
+                                            <small>80+ Completed</small>
+                                        </article>
+                                    </div>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Culpa deserunt ipsam itaque
+                                        maiores provident quia similique voluptas! Ad
+                                        adipisci dolorem, dolores eaque nemo nulla
+                                        perspiciatis praesentium repudiandae suscipit
+                                        tempore vel!
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
